a34fbc9306d040bc3639669b18fb06eb8cc7c7e8
Pedro Ligabue <pliga@globo.com>
2020-09-26 02:59:20 -0300

    Create postal info service
    This service uses ViaCEP's API to fetch addresses based on CEPs.

---
+++ src/services/postal_info.py
@@ -0,0 +1,24 @@
+import requests
+import re
+
+from db import Address
+
+def get_postal_info(postal_code):
+    r = requests.get(f"https://viacep.com.br/ws/{postal_code}/json")
+    r_json = r.json()
+
+    return {
+        "postal_code": re.sub('[^0-9]','', r_json["cep"]),
+        "street": r_json["logradouro"],
+        "number": r_json["complemento"],
+        "neighbourhood": r_json["bairro"],
+        "city": r_json["localidade"],
+        "state": r_json["uf"]
+    }
+
+def build_address(postal_code):
+    address = Address(country="BR")
+    postal_info = get_postal_info(postal_code)
+    address.from_dict(postal_info)
+
+    return address

991a34bc47cf05f6a08a8a87edb91360caf48151
Bruno <brunob.inacio@gmail.com>
2020-10-10 21:14:44 -0300

    Update helpers and services to conform to PEP8

--- src/helpers/address_power_supply.py
+++ src/helpers/address_power_supply.py
@@ -1,25 +1,24 @@
 from db import session, User, PowerSupply, Address
 from services.postal_info import build_address
 
-def get_address_power_supply(address):
 
-    ps = session.query(PowerSupply).\
-        join(PowerSupply.address).\
-        filter(Address.country == address.country,
-               Address.state == address.state,
-               Address.city == address.city,
-               Address.neighbourhood == address.neighbourhood).one_or_none()
+def get_address_power_supply(address):
+    ps = session.query(PowerSupply)\
+         .join(PowerSupply.address)\
+         .filter(Address.country == address.country,
+                 Address.state == address.state,
+                 Address.city == address.city,
+                 Address.neighbourhood == address.neighbourhood)\
+         .one_or_none()
 
     return ps
 
-def get_postal_code_power_supply(postal_code):
 
+def get_postal_code_power_supply(postal_code):
     address = build_address(postal_code)
-    
     return get_address_power_supply(address)
 
+
 def get_user_power_supply(user):
-
     address = user.address
-
     return get_address_power_supply(address)
--- src/services/postal_info.py
+++ src/services/postal_info.py
@@ -3,18 +3,20 @@ import re
 
 from db import Address
 
+
 def get_postal_info(postal_code):
     r = requests.get(f"https://viacep.com.br/ws/{postal_code}/json")
     r_json = r.json()
 
     return {
-        "postal_code": re.sub('[^0-9]','', r_json["cep"]),
+        "postal_code": re.sub('[^0-9]', '', r_json["cep"]),
         "street": r_json["logradouro"],
         "number": r_json["complemento"],
         "neighbourhood": r_json["bairro"],
         "city": r_json["localidade"],
         "state": r_json["uf"]
     }
+
 
 def build_address(postal_code):
     address = Address(country="BR")

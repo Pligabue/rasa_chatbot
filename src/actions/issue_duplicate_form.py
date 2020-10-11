8cb2f2dc8c78780c5e61e21e25aa46c0e80e7a5f
Bruno <brunob.inacio@gmail.com>
2020-10-10 21:59:45 -0300

    Update actions to conform to PEP8

--- src/actions/issue_duplicate_form.py
+++ src/actions/issue_duplicate_form.py
@@ -9,6 +9,7 @@ from rasa_sdk.executor import CollectingDispatcher
 
 from db import Session, User
 
+
 class IssueDuplicateForm(FormAction):
 
     def name(self) -> Text:
@@ -25,83 +26,92 @@ class IssueDuplicateForm(FormAction):
             "month": self.from_entity(entity="month"),
         }
 
-    def submit(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict]:
+    def submit(self,
+               dispatcher: CollectingDispatcher,
+               tracker: Tracker,
+               domain: Dict[Text, Any]) -> List[Dict]:
+
         document = tracker.get_slot("cpf")
         month = tracker.get_slot("month")
         year = tracker.get_slot("year")
 
         session = Session()
+        user = (session.query(User)
+                .filter(User.document == document)
+                .one_or_none())
 
-        user = session.query(User).filter(User.document == document).one_or_none()
         if user is None:
             dispatcher.utter_message(template="utter_no_document_match")
             return []
 
-        bills = filter(lambda bill: (bill.due_date.month == month and bill.due_date.year == year), user.bills)
+        bills = filter(lambda bill: (bill.due_date.month == month
+                                     and bill.due_date.year == year),
+                       user.bills)
         if not bills:
             dispatcher.utter_message(template="utter_no_bills")
             return []
 
         for bill in bills:
             is_past = bill.due_date < datetime.date.today()
-            paid = bill.paid
-            template = f"utter_{'past' if is_past else 'future'}_{'paid' if paid else 'unpaid'}_bill"
             amount = f"{bill.value:_.2f}".replace(".", ",").replace("_", ".")
             date = f"{bill.due_date.strftime('%d/%m/%Y')}"
-            dispatcher.utter_message(template=template, amount=amount, date=date)      
+            template = (f"utter_{'past' if is_past else 'future'}_"
+                        f"{'' if bill.paid else 'un'}paid_bill")
+
+            dispatcher.utter_message(template=template,
+                                     amount=amount,
+                                     date=date)
+
         return []
 
-    def validate_cpf(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
+    def validate_cpf(self,
+                     value: Text,
+                     dispatcher: CollectingDispatcher,
+                     tracker: Tracker,
+                     domain: Dict[Text, Any]) -> Dict[Text, Any]:
+
         print(f"CPF IS {value}")
+
         cpf = re.sub(r"[^\d]", "", value)
+
         if len(cpf) == 11:
-            return { "cpf": cpf }
+            return {"cpf": cpf}
         else:
             dispatcher.utter_message(template="utter_invalid_cpf")
-            return { "cpf": None }
+            return {"cpf": None}
 
-    def validate_month(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        month_name = value.lower()    
+    def validate_month(self,
+                       value: Text,
+                       dispatcher: CollectingDispatcher,
+                       tracker: Tracker,
+                       domain: Dict[Text, Any]) -> Dict[Text, Any]:
+
         months = ["janeiro", "fevereiro", "março", 
                   "abril", "maio", "junho",
                   "julho", "agosto", "setembro", 
                   "outubro", "novembro", "dezembro"]
-        matches = difflib.get_close_matches(month_name, months)
+
+        matches = difflib.get_close_matches(value.lower(), months)
+
         if not matches:
-            return { "month": None }
+            return {"month": None}
+
         print(f"match is {months.index(matches[0])}")
         month_num = months.index(matches[0]) + 1
         print(f"month is {month_num}")
-        return { "month": month_num }
+        return {"month": month_num}
 
-    def validate_year(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
+    def validate_year(self,
+                      value: Text,
+                      dispatcher: CollectingDispatcher,
+                      tracker: Tracker,
+                      domain: Dict[Text, Any]) -> Dict[Text, Any]:
         try:
-            year = int(value)    
+            year = int(value)
             if year > 2000:
-                return { "year": year }
-            return { "year": None }
+                return {"year": year}
+
         except ValueError:
-            return { "year": None }
+            return {"year": None}
+
+        return {"year": None}

d076ac403e3fa76ef52e53e527078718dc0d174f
Pedro Ligabue <pliga@globo.com>
2020-10-25 01:56:57 -0300

    Update domain to include FAQ

--- src/domains/__init__.py
+++ src/domains/__init__.py
@@ -3,6 +3,7 @@ from os import listdir, path
 from pathlib import Path
 import yaml
 
+
 def run():
     current_dir = Path(__file__).resolve().parent
 
@@ -21,22 +22,25 @@ def run():
         "slots": {},
         "responses": {},
         "forms": [],
-        "session_config": {}
+        "session_config": {},
+        "actions": []
     }
 
     def merge_domains(previous_dom, current_dom):
         for key in current_dom:
-            if key in ["intents", "entities", "forms"]:
-                previous_dom[key] = list(sorted(set(previous_dom[key] + current_dom[key]), key=str.lower))
+            if key in ["intents", "entities", "forms", "actions"]:
+                previous_dom[key] = list(
+                    sorted(set(previous_dom[key] + current_dom[key]), key=str.lower))
             elif key in ["slots", "responses", "session_config"]:
                 previous_dom[key] = {**previous_dom[key], **current_dom[key]}
         return previous_dom
 
     final_domain = reduce(merge_domains, partial_domains, initial_domain)
-    
+
     destination_file = current_dir.parent / "domain.yml"
     with open(destination_file, "w") as file:
         yaml.dump(final_domain, file, allow_unicode=True)
+
 
 if __name__ == "__main__":
     run()
